3e45bc5e653ec22bcb0e2e96a76e18e4b7d4af45
Rada Berar <ujagaga@gmail.com>
2026-01-08 19:49:22 +0100

    file logging configurable

--- TcpServer/image_server.py
+++ TcpServer/image_server.py
@@ -52,6 +52,16 @@ def setup_logger():
         console_handler.setLevel(logging.DEBUG)  # Ensure handler shows debug
         console_handler.setFormatter(formatter)
         logger_obj.addHandler(console_handler)
+    else:
+        if config.DEBUG_TO_FILE:
+            file_handler = RotatingFileHandler(
+                os.path.join(os.path.dirname(__file__), 'app.log'),
+                maxBytes=65535,
+                backupCount=1
+            )
+            file_handler.setLevel(logging.INFO)  # Only log info and above
+            file_handler.setFormatter(formatter)
+            logger_obj.addHandler(file_handler)
 
     return logger_obj
 
